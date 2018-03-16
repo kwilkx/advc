b97f71069e1bd5bbb4b4a700907467659f50f655
Your Name <you@example.com>
2018-03-16 10:52:48 +0100

    added templates

---
+++ templates/operator_vector.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include <sstream>
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& container) {
+    return os;
+}
+
+TEST(TemplatedOperator, emptyVector) {
+    std::vector<std::string> empty;
+    std::stringstream ss;
+
+    ss << empty;
+
+    EXPECT_EQ(ss.str(), "");
+}
+
+TEST(TemplatedOperator, stringVector) {
+    std::vector<std::string> data{{"a", "b", "c"}};
+    std::stringstream ss;
+
+    ss << data;
+
+    EXPECT_EQ(ss.str(), "abc");
+}
